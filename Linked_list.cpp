0dd1c9e55de14288db2c1466a08a618cd879f856
adityandroid <adityandroid@gmail.com>
2016-11-06 15:10:04 +0530

    Linked List.cpp deletion code added

--- Linked_list.cpp
+++ Linked_list.cpp
@@ -29,6 +29,36 @@ void create(int ip){
 	}	
 	
 }
+void deleteN(int ip){
+	node *temp,*prev;
+	temp = start;
+	while(temp !=NULL){
+		if(temp->data == ip){
+			if (temp == start){			//deleting starting node
+				start = temp->next;
+				delete temp;
+				return;
+			}
+			else{
+				prev = start;				//other cases (middle+last)
+				while(prev->next != temp)
+					prev = prev->next;		//reached previous node
+				 prev->next = temp->next;
+				 delete temp;
+				 return;
+			}
+		}
+		else
+			temp = temp->next;
+	}
+}
+void display(){
+	node *temp = start;
+	while(temp!=NULL){
+		cout<<temp->data<<endl;
+		temp = temp->next;
+	}	
+}
 int main(){
 	int n,ip;
 	cin>>n;
@@ -38,10 +68,9 @@ int main(){
 		cin>>ip;
 		create(ip);
 	}
-	node *temp = start;
-	while(temp!=NULL){
-		cout<<temp->data<<endl;
-		temp = temp->next;
-	}
+	display();
+	cout<<"deleted 4\n";
+	deleteN(4);
+	display();
 	return 0;
 }
